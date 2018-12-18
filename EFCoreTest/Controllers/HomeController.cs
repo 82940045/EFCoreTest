0b8ae35fef5e902776953e95ba7a2fee40fa9d8c
82940045 <82940045@qq.com>
2018-12-18 15:20:26 +0800

    添加Kafka消费测试项目

--- EFCoreTest/Controllers/HomeController.cs
+++ EFCoreTest/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EFCoreTest.Models;
-using EFCoreTest.DataAccessModels;
 using EFCoreTest.DBMySQLModels;
 
 namespace EFCoreTest.Controllers
